ebcec0c1696c5d74efce98612990b9aac5c46a71
haneybot2 <41707988+haneybot2@users.noreply.github.com>
2018-08-05 22:05:37 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -245,7 +245,6 @@ function play(guild, song) {
 
 client.on('message', message => {
   if (message.content === '!join') {
-		    if(!message.member.hasPermission("MANAGE_MESSAGES"))
     const channel = message.member.voiceChannel;
 
     channel.join()
