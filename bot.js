dd3ebed6105356e748f0079b08ac693b09f5881e
haneybot2 <41707988+haneybot2@users.noreply.github.com>
2018-08-29 18:25:12 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -216,7 +216,7 @@ async function handleVideo(video, msg, voiceChannel, playlist = false) {
 		serverQueue.songs.push(song);
 		console.log(serverQueue.songs);
 		if (playlist) return undefined;
-		else return msg.channel.send("✅ `${playlist.title}` Added to **.A-Queue**!");
+		else return msg.channel.send("✅ `${song.title}` Added to **.A-Queue**!");
 	}
 	return undefined;
 }
