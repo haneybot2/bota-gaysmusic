7ff73a308a123a8eef0feadb23ef8d40d7e35fc5
haneybot2 <41707988+haneybot2@users.noreply.github.com>
2018-09-11 15:14:08 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -198,7 +198,7 @@ async function handleVideo(video, msg, voiceChannel, playlist = false) {
             queueConstruct.songs.push(song);
 
             try {
-		msg.delete(20000);
+		msg.delete();
                 var connection = await voiceChannel.join();
                 queueConstruct.connection = connection;
                 play(msg.guild, queueConstruct.songs[0]);
